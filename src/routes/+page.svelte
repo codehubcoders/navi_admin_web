393e89b92beeee0ed1c5a9b39f2817fda5bc0fac
sisily11 <sisily11@naver.com>
2024-01-09 12:22:18 +0300

    scss 및 기본 레이아웃

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="bold fs-22">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <style lang="scss">
+
+
 
 
 </style>
